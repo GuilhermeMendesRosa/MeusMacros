ff6123bdc31b3fbd02755d3f187cc4467562055c
GuilhermeMendesRosa <guilhermemr.jlle@gmail.com>
2025-01-17 16:38:27 -0300

    criando outros serviços

---
+++ meus-macros-back/ai-meus-macros/src/main/java/br/com/roselabs/ai_meus_macros/AiMeusMacrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.roselabs.ai_meus_macros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMeusMacrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMeusMacrosApplication.class, args);
+	}
+
+}
---
+++ meus-macros-back/ai-meus-macros/src/test/java/br/com/roselabs/ai_meus_macros/AiMeusMacrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roselabs.ai_meus_macros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMeusMacrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
