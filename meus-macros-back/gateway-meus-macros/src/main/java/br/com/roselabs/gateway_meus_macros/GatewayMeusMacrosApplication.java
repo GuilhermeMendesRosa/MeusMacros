8a9ba612a2ae496d676b0c4fb3b95ae6795c617b
GuilhermeMendesRosa <guilhermemr.jlle@gmail.com>
2025-01-16 21:12:36 -0300

    Criando gateway

---
+++ meus-macros-back/gateway-meus-macros/src/main/java/br/com/roselabs/gateway_meus_macros/GatewayMeusMacrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.roselabs.gateway_meus_macros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMeusMacrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMeusMacrosApplication.class, args);
+	}
+
+}
---
+++ meus-macros-back/gateway-meus-macros/src/test/java/br/com/roselabs/gateway_meus_macros/GatewayMeusMacrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roselabs.gateway_meus_macros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMeusMacrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
