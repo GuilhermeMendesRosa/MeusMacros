1a2a5e39e4e74a141672326ae99f33d0bb07692a
GuilhermeMendesRosa <guilhermemr.jlle@gmail.com>
2025-01-21 09:47:36 -0300

    adicionando capacitor para gerar aplicativo mobile

---
+++ meus-macros-front/android/app/src/main/java/br/com/roselabs/MainActivity.java
@@ -0,0 +1,5 @@
+package br.com.roselabs;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ meus-macros-front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.roselabs',
+  appName: 'meus-macros-front',
+  webDir: 'dist/meus-macros-front/browser',
+};
+
+export default config;
