a05042e33cea95771698e7ae1425fe89eccbc655
GuilhermeMendesRosa <guilhermemr.jlle@gmail.com>
2025-02-18 23:15:34 -0300

    ajuste macros calculators

--- meus-macros-back/macros-calculator-meus-macros/src/main/resources/db/migration/V1__create_meals.sql
+++ meus-macros-back/macros-calculator-meus-macros/src/main/resources/db/migration/V1__create_meals.sql
@@ -1,23 +1,26 @@
-CREATE TABLE meals (
-    id BIGSERIAL PRIMARY KEY,
-    user_uuid UUID NOT NULL,
-    date TIMESTAMP WITH TIME ZONE NOT NULL,
-    calories INT NOT NULL,
-    protein INT NOT NULL,
-    carbohydrates INT NOT NULL,
-    fat INT NOT NULL
+CREATE TABLE meals
+(
+    id            BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_uuid     CHAR(36) NOT NULL,
+    date          DATETIME NOT NULL,
+    calories      INT      NOT NULL,
+    protein       INT      NOT NULL,
+    carbohydrates INT      NOT NULL,
+    fat           INT      NOT NULL
 );
 
-CREATE TABLE meal_items (
-    id BIGSERIAL PRIMARY KEY,
-    meal_id BIGINT NOT NULL REFERENCES meals(id) ON DELETE CASCADE,
-    name VARCHAR(255) NOT NULL,
-    quantity VARCHAR(50) NOT NULL,
-    calories INT NOT NULL,
-    protein INT NOT NULL,
-    carbohydrates INT NOT NULL,
-    fat INT NOT NULL
+CREATE TABLE meal_items
+(
+    id            BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    meal_id       BIGINT UNSIGNED NOT NULL,
+    name          VARCHAR(255) NOT NULL,
+    quantity      VARCHAR(50)  NOT NULL,
+    calories      INT          NOT NULL,
+    protein       INT          NOT NULL,
+    carbohydrates INT          NOT NULL,
+    fat           INT          NOT NULL,
+    FOREIGN KEY (meal_id) REFERENCES meals (id) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_meals_user_uuid ON meals(user_uuid);
-CREATE INDEX idx_meal_items_meal_id ON meal_items(meal_id);
+CREATE INDEX idx_meals_user_uuid ON meals (user_uuid);
+CREATE INDEX idx_meal_items_meal_id ON meal_items (meal_id);
