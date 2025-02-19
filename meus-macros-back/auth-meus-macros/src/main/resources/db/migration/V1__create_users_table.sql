dbfece5caaf32b64d593c535950b7b6a8453a1cf
GuilhermeMendesRosa <guilhermemr.jlle@gmail.com>
2025-02-18 22:56:57 -0300

    ajuste mysql

--- meus-macros-back/auth-meus-macros/src/main/resources/db/migration/V1__create_users_table.sql
+++ meus-macros-back/auth-meus-macros/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id         SERIAL PRIMARY KEY,
+    id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
