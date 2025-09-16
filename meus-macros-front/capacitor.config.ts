09d6a2bad23a2f492689f425c87db1fd28c11b95
Guilherme Mendes <83733948+GuilhermeMendesRosa@users.noreply.github.com>
2025-09-16 14:59:02 -0300

    ajustes para rodar app

--- meus-macros-front/capacitor.config.ts
+++ meus-macros-front/capacitor.config.ts
@@ -1,9 +1,35 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'br.com.roselabs',
-  appName: 'meus-macros-front',
+  appId: 'br.com.roselabs.meusmacros',
+  appName: 'Meus Macros',
   webDir: 'dist/meus-macros-front/browser',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#10b981',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    },
+    StatusBar: {
+      style: 'light',
+      backgroundColor: '#10b981',
+      overlaysWebView: false
+    },
+    Keyboard: {
+      resize: 'ionic'
+    }
+  },
+  ios: {
+    contentInset: 'automatic',
+    scheme: 'App',
+    allowsLinkPreview: false,
+    scrollEnabled: true
+  }
 };
 
 export default config;
